03acb57bcd60deee48a41e84e012c5c7b0500ec0
Dhineshkumar <dhineshkumar.velayudham@ccubic.in>
2024-09-17 19:27:16 +0530

    RegexEmail

---
+++ demo/src/Regex.java
@@ -0,0 +1,17 @@
+
+    import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+    public class Regex {
+        boolean isValidEmail(String email){
+            String regex="^(\\w+)@(\\w+)\\.(.+)$";
+            return email.matches(regex);
+        }
+        public static void main(String[] args) {
+            Regex r=new Regex();
+            System.out.println(r.isValidEmail("ssuthagar277@gma.c@m"));
+            System.out.println(r.isValidEmail("ssuthagar277@gmail.fgh"));
+            System.out.println(r.isValidEmail("ssuthagar277@gmail.com"));
+        }
+    }
+
